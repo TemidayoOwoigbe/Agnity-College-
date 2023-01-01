91ce10d5ad478cc14af4fa8bbe3469509781427f
TemidayoOwoigbe <113631399+TemidayoOwoigbe@users.noreply.github.com>
2023-01-01 12:17:24 +0100

    app.js

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ function open_nav() {
 function close_nav() {
     document.querySelector('#open-btn').style.display = 'block';
     document.querySelector('#close-btn').style.display = 'none';
-    document.querySelector('#menu').style.height = '60px';
+    document.querySelector('#menu').style.height = '50px';
 }
